62f74b86b2778618f4e2350ad2430361a0d63433
syyyym <ameliasophiehayes@gmail.com>
2019-10-05 18:13:03 +0000

    css styling for pages

--- src/App.js
+++ src/App.js
@@ -14,10 +14,12 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/articles-list" component={ArticlesList} />
-        <Route path="/article" component={ArticlePage} />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/articles-list" component={ArticlesList} />
+          <Route path="/article" component={ArticlePage} />
+        </div>
       </div>
     </Router>
   );
