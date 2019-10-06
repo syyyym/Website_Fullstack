99dead442dae29d85f9462308060a8840a01d05a
syyyym <ameliasophiehayes@gmail.com>
2019-10-06 12:25:15 +0000

    use URL parameter in route path of Route component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,12 @@
- import React from 'react';
+import React from 'react';
 
-const ArticlePage = () => (
-    <>
-    <h1>This is an article</h1>
-    </>
-);
+const ArticlePage = ({ match }) => {
+    const name = match.params.name;
+    return (
+        <>
+            <h1>This is the {name} article</h1>
+        </>
+    );
+};
 
 export default ArticlePage;
