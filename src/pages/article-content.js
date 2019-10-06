3aa12dcb928e492299dc32792ddc1c7c84a42789
syyyym <ameliasophiehayes@gmail.com>
2019-10-06 16:51:22 +0000

    add links to articles in ArticlesList page component

--- src/pages/article-content.js
+++ src/pages/article-content.js
@@ -23,7 +23,8 @@ const articles = [
             interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
             consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
         ]
-    },    {
+    },
+    {
         name: 'learn-node',
         title: 'How to Build a Node Server in 10 Minutes',
         content: [
@@ -47,12 +48,12 @@ const articles = [
             interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
             consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
         ]
-    },     {
-        name: 'my-thoughts-on-resumes',
-        title: 'My Thoughts on Resumes',
+    },
+    {
+        name: 'lear-javascript',
+        title: 'Learning Javascript language',
         content: [
-            `Today is the day I talk about something which scares most people: resumes.
-            In reality, I'm not sure why people have such a hard time with proin congue
+            `This is an article abot learning javascript language with proin congue
             ligula id risus posuere, vel eleifend ex egestas. Sed in turpis leo. 
             Aliquam malesuada in massa tincidunt egestas. Nam consectetur varius turpis, 
             non porta arcu porttitor non. In tincidunt vulputate nulla quis egestas. Ut 
@@ -71,7 +72,7 @@ const articles = [
             interdum, nibh elit congue tellus, ac vulputate urna lorem nec nisi. Morbi id 
             consequat quam. Vivamus accumsan dui in facilisis aliquet.`,
         ]
-    },  
+    },
 ];
 
 export default articles;
