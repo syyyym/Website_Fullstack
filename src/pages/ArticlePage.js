43925fb228850ed64cfbb31b4ae38465d0e9ffb5
syyyym <ameliasophiehayes@gmail.com>
2019-10-06 20:44:55 +0000

    add a list of other articles to ArticlePage

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ArticlesList from '../components/ArticlesList';
 import articleContent from './article-content';
 
 const ArticlePage = ({ match }) => {
@@ -6,6 +7,8 @@ const ArticlePage = ({ match }) => {
     const article = articleContent.find(article => article.name === name);
 
     if (!article) return <h1>Article does not exist!</h1>
+
+    const otherArticles = articleContent.filter(article => article.name !== name);
     
     return (
         <>
@@ -13,6 +16,8 @@ const ArticlePage = ({ match }) => {
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
+        <h3>Other Articles:</h3>
+        <ArticlesList articles={otherArticles} />
         </>
     );
 };
