8dfa5330f2f21e599b320582620cc652ebeb32d7
syyyym <ameliasophiehayes@gmail.com>
2019-10-07 19:40:08 +0000

    add UpvetesSection comp, button for updating votes

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
+import UpvotesSection from '../components/UpvotesSection';
 import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
@@ -30,7 +31,7 @@ const ArticlePage = ({ match }) => {
     return (
         <>
             <h1>{article.title}</h1>
-            <p>This post has been upvoted {articleInfo.upvotes} times</p>
+            <UpvotesSection articleName={name} upvotes={articleInfo.upvotes} setArticleInfo={setArticleInfo} />
             {article.content.map((paragraph, key) => (
                 <p key={key}>{paragraph}</p>
             ))}
