473579053c12f88ec2ca108a70422c4bdcbec8b0
syyyym <ameliasophiehayes@gmail.com>
2019-10-07 15:49:59 +0000

    update db with the update endpoint post req info

--- backend/src/server.js
+++ backend/src/server.js
@@ -22,11 +22,26 @@ app.get('/api/articles/:name', async (req, res) => {
     
 })
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-    const articleName = req.params.name;
-
-    articlesInfo[articleName].upvotes += 1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    try {
+        const articleName = req.params.name;
+        const client = await MongoClient.connect(config.dbURI, { useNewUrlParser: true });
+        const db = client.db('my-website');
+    
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': {
+                upvotes: articleInfo.upvotes + 1,
+            },
+        });
+        const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    
+        res.status(200).json(updatedArticleInfo);
+    
+        client.close();
+    } catch (error) {
+        res.status(500).json({ message: 'Error connecting to db', error });
+    }
 });
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
