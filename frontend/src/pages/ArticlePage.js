0686388f3e6979657fa71058cb4c8ce947fd081e
syyyym <ameliasophiehayes@gmail.com>
2019-10-07 20:12:20 +0000

    add comment form comp, update Db

--- frontend/src/pages/ArticlePage.js
+++ frontend/src/pages/ArticlePage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import UpvotesSection from '../components/UpvotesSection';
 import CommentsList from '../components/CommentsList';
+import AddCommentForm from '../components/AddCommentForm';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
@@ -36,6 +37,7 @@ const ArticlePage = ({ match }) => {
                 <p key={key}>{paragraph}</p>
             ))}
         <CommentsList comments={articleInfo.comments} />
+        <AddCommentForm articleName={name} setArticleInfo={setArticleInfo} />
         <h3>Other Articles:</h3>
         <ArticlesList articles={otherArticles} />
         </>
